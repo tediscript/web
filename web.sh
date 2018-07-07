35ce399581f1f88838e0bbc8357bfc059cfab380
tediscript <tediscript@gmail.com>
2018-07-07 20:16:05 +0700

    update db script

--- web.sh
+++ web.sh
@@ -45,10 +45,11 @@ password=${pass}" > /var/www/${1}/conf/mysql.conf
     #create database
     . /etc/web/mysql.conf
     mysql --user=root --password=${password} <<_EOF_
-    CREATE USER '${name}'@'localhost' IDENTIFIED WITH mysql_native_password AS '${pass}';
+    CREATE USER '${name}'@'localhost' IDENTIFIED BY '${pass}';
     GRANT USAGE ON *.* TO '${name}'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
-    CREATE DATABASE IF NOT EXISTS `${name}`;GRANT ALL PRIVILEGES ON `${name}`.* TO '${name}'@'localhost';
-    GRANT ALL PRIVILEGES ON `${name}\_%`.* TO '${name}'@'localhost';
+    CREATE DATABASE IF NOT EXISTS `${name}`;
+    GRANT ALL PRIVILEGES ON `${name}`.* TO '${name}'@'localhost';
+    FLUSH PRIVILEGES;
 _EOF_
 }
 
