69aa92f32ab6f2aba2eb26b2c403d2e6396ee3cb
tediscript <tediscript@gmail.com>
2018-08-08 12:10:05 +0700

    update to v0.6.0-dev

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "##################################"
-echo "###      WebEngine v0.5.3      ###"
+echo "###      WebEngine v0.6.0-dev      ###"
 echo "##################################"
 
 function is_valid_email() {
